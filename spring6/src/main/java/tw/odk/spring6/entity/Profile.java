a0d2f980861542d2ee8586f268955e757be92491
Mkie401 <b23851520@gmail.com>
2025-11-11 09:35:12 +0800

    11110934

---
+++ spring6/src/main/java/tw/odk/spring6/entity/Profile.java
@@ -0,0 +1,41 @@
+package tw.odk.spring6.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "profile")
+public class Profile {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String cname;
+	private Integer age;
+	
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getCname() {
+		return cname;
+	}
+	public void setCname(String cname) {
+		this.cname = cname;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
+	
+	
+}
