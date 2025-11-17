218220104e57ceef341a752aa60d9ad8bdc2091e
Mkie401 <b23851520@gmail.com>
2025-11-17 10:10:28 +0800

    11171010

---
+++ spring10/src/main/resources/static/js/myjs.js
@@ -0,0 +1,49 @@
+window.onload = function(){
+	let start = document.getElementById("start");
+	let chatDiv = document.getElementById("chatDiv");
+	let mesg = document.getElementById("mesg");
+	let send = document.getElementById("send");
+	let log = document.getElementById("log");
+	let webSocket;
+	
+	start.style.display = "block";
+	chatDiv.style.display = "none";
+	
+	start.addEventListener("click", function(){
+		connect("ws://10.0.103.207:8080/AWeb/myserver");
+	});
+	
+	send.addEventListener("click", function(){
+		let message = {
+			message: mesg.value
+		};
+		webSocket.send(JSON.stringify(message));
+	});
+	
+	function connect(url){
+		webSocket = new WebSocket(url);
+		
+		webSocket.onopen = function(){
+			console.log("onopen")
+			
+			start.style.display = "none";
+			chatDiv.style.display = "block";
+		}
+		
+		webSocket.onmessage = function(event){
+			console.log(event.data);
+			let mesgObj = JSON.parse(event.data);
+			log.innerHTML += mesgObj.message + "<br>";
+		}
+		
+		webSocket.onclose = function(){
+			console.log("onclose")
+			start.style.display = "block";
+			chatDiv.style.display = "none";
+		}
+		
+		webSocket.onerror = function(event){
+			console.log("onerror");
+		}
+	}
+}
---
+++ spring10/src/main/resources/static/js/student.js
@@ -0,0 +1,46 @@
+window.onload = function(){
+	let canvas, ctx;
+	let webSocket = new WebSocket("ws://10.0.103.207:8080/ws");
+	let isConnect = false;
+	
+	webSocket.onopen = function(){
+		isConnect = true;
+	};
+	webSocket.onmessage = function(event){
+		if (isConnect){
+			let mesgObj = JSON.parse(event.data);
+			if (mesgObj.isClear){
+				clear();
+			}else{
+				if (mesgObj.isNewline){
+					newLine(mesgObj.x, mesgObj.y);
+				}else{
+					drawLine(mesgObj.x, mesgObj.y);
+				}
+			}
+		}
+	}
+	webSocket.onclose = function(){
+		isConnect = false;
+	};
+	webSocket.onerror = function(e){
+		console.log("ERROR:" + e.data);
+	};
+	
+	canvas = document.getElementById("myDrawer");
+	ctx = canvas.getContext("2d");
+	
+	function clear(){
+		ctx.clearRect(0,0,canvas.width, canvas.height);
+	}
+	function newLine(x,y){
+		ctx.beginPath();
+		ctx.lineWidth = 4;
+		ctx.moveTo(x, y);		
+	}
+	function drawLine(x,y){
+		ctx.lineTo(x, y);
+		ctx.stroke();		
+	}
+	
+}
